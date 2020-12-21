d04a38c1d331452d97dda73fa56910f53377f65c
ISs25u <11806309+ISs25u@users.noreply.github.com>
2020-12-21 23:28:45 +0100

    next

--- config.h
+++ config.h
@@ -3,21 +3,20 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 6;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 6;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 12;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 12;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 12;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 12;        /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"monospace:size=10:pixelsize=13:antialias=true:autohint=true"};
-static char dmenufont[]             = "monospace:size=10";
+static char dmenufont[]             = "monospace:size=10:pixelsize=13:antialias=true:autohint=true";
 
 static char normfgcolor[]           = "#00FFFF";
 static char normbgcolor[]           = "#FF00FF";
 static char normbordercolor[]       = "#FFFF00";
-
 
 static char selfgcolor[]            = "#FF0000";
 static char selbgcolor[]            = "#00FF00";
