d6fbae622fc3de5c11356a3c83fd8608ace11239
ISs25u <11806309+ISs25u@users.noreply.github.com>
2020-12-21 23:06:22 +0100

    fonts

--- config.h
+++ config.h
@@ -11,23 +11,24 @@ static const int swallowfloating    = 1;        /* 1 means swallow floating wind
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"monospace:size=10", "Twitter Color Emoji:pixelsize=12:antialias=true:autohint=true", "FontAwesome:pixelsize=13:antialias=true:autohint=true", "Font Awesome 5 Brands:pixelsize=13:antialias=true:autohint=true", "Font Awesome 5 Free:pixelsize=13:antialias=true:autohint=true"};
+static const char *fonts[]          = {"monospace:size=10:pixelsize=13:antialias=true:autohint=true"};
 static char dmenufont[]             = "monospace:size=10";
 
 static char normfgcolor[]           = "#00FFFF";
 static char normbgcolor[]           = "#FF00FF";
 static char normbordercolor[]       = "#FFFF00";
 
-//static char normbgcolor[]           = "#222222";
-//static char normbordercolor[]       = "#444444";
-//static char normfgcolor[]           = "#bbbbbb";
 
 static char selfgcolor[]            = "#FF0000";
 static char selbgcolor[]            = "#00FF00";
 static char selbordercolor[]        = "#0000FF";
+
 //static char selfgcolor[]            = "#eeeeee";
 //static char selbordercolor[]        = "#883333";
 //static char selfgcolor[]            = "#eeeeee";
+//static char normbgcolor[]           = "#222222";
+//static char normbordercolor[]       = "#444444";
+//static char normfgcolor[]           = "#bbbbbb";
 
 static char *colors[][3] = {
        /*               fg           bg           border   */
